bb4385d64b6ab8ccf1783f7f24fffaf2328c1491
DV8FromTheWorld <keeneraustin@yahoo.com>
2014-05-21 05:03:30 -0400

    Added a Custom screen capture. Possible bug:  Doesn't show the button to capture for all people.

---
+++ Imgur Uploader/ScreenCapture.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Screen_Capture
+{
+    partial class ScreenCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenCapture));
+            this.btnToClipboard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToClipboard
+            // 
+            this.btnToClipboard.Location = new System.Drawing.Point(82, 242);
+            this.btnToClipboard.Name = "btnToClipboard";
+            this.btnToClipboard.Size = new System.Drawing.Size(116, 20);
+            this.btnToClipboard.TabIndex = 0;
+            this.btnToClipboard.Text = "Copy To Clipboard";
+            this.btnToClipboard.UseVisualStyleBackColor = true;
+            this.btnToClipboard.Click += new System.EventHandler(this.btnToClipboard_Click);
+            // 
+            // ScreenCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnToClipboard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "ScreenCapture";
+            this.Text = "Screen Capture";
+            this.Load += new System.EventHandler(this.ScreenCapture_Load);
+            this.Resize += new System.EventHandler(this.ScreenCapture_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnToClipboard;
+
+
+    }
+}
+
---
+++ Imgur Uploader/ScreenCapture.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Screen_Capture
+{
+    public partial class ScreenCapture : Form
+    {
+        private Size oldSize;
+        public ScreenCapture()
+        {
+            oldSize = Size;
+            InitializeComponent();
+        }
+
+        private void ScreenCapture_Load(object sender, EventArgs e)
+        {
+            ResizeForm();
+        }
+
+        private void ScreenCapture_Resize(object sender, EventArgs e)
+        {
+            ResizeForm();
+        }
+
+        private void ResizeForm()
+        {
+            Rectangle rect = new Rectangle(new Point(0, 0), this.Size);
+            Region region = new Region(rect);
+
+            rect.Inflate(-1 * (this.Width - 7), -1 * (this.Height - 31));
+            region.Exclude(rect);
+
+            this.Region = region;
+            moveButton();
+            oldSize = Size;
+        }
+
+        private void moveButton()
+        {
+            int yDelta = Size.Height - oldSize.Height;
+            btnToClipboard.Location = new Point(
+                (Size.Width / 2) - (btnToClipboard.Width / 2),
+                btnToClipboard.Location.Y + yDelta);
+        }
+
+        private void btnToClipboard_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(Location.X + 7 , Location.Y + 31, Size.Width - 14, Size.Height - 62);
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            Clipboard.SetImage(bmp);
+        }
+    }
+}
